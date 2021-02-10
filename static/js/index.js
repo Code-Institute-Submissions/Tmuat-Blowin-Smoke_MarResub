867e438f4d72e3f84ae9ccea14280213d2ebe446
Tmuat <Thomas_Muat@hotmail.com>
2021-02-10 14:16:44 +0000

    Added AOS for index.html

--- static/js/index.js
+++ static/js/index.js
@@ -48,3 +48,11 @@ $(document).ready(function () {
 
     });
 });
+
+// Script to start the AOS library from https://michalsnik.github.io/aos/
+
+// Starts the animation on scroll library 
+AOS.init({
+    // Sets the duration of the animations
+    duration: 1000,
+});
