672fec44078161a12dbd0924140dc700c30425eb
Tmuat <Thomas_Muat@hotmail.com>
2021-02-11 13:27:57 +0000

    Added client side validation

---
+++ static/js/register.js
@@ -0,0 +1,47 @@
+// Code from https://www.sitepoint.com/basic-jquery-form-validation-tutorial/
+$(function () {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("form[name='registration']").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            firstname: "required",
+            lastname: "required",
+            email: {
+                required: true,
+                // Specify that email should be validated
+                // by the built-in "email" rule
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 5
+            },
+            password2: {
+                minlength: 5,
+                equalTo: "#password"
+            },
+        },
+        // Specify validation error messages
+        messages: {
+            firstname: "Please enter your firstname",
+            lastname: "Please enter your lastname",
+            email: "Please enter a valid email address",
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+            password2: {
+                equalTo: "The password don't match!"
+            },
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+});
