4d1dc24c9045959dd1cd20598e0de02e129ed8a9
Tmuat <Thomas_Muat@hotmail.com>
2021-02-11 15:17:06 +0000

    Added toast & adjusted navbar scroll vh %

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // When the user scrolls down 30px from the top of the document, add background to navbar
 $(window).scroll(function () {
     let scroll = $(window).scrollTop();    
-    if (scroll >= 0.4 * $(window).height()) {
+    if (scroll >= 0.25 * $(window).height()) {
         $('.navbar').addClass('bg-black shadow');
     } else {
         $('.navbar').removeClass('bg-black shadow');
@@ -13,4 +13,6 @@ window.onload = function () {
     $(".navbar-toggler").click(function () {
         $(".navbar").toggleClass("bg-black-extra");
     });
+
+    $(".toast").toast("show");
 };
