32b609bfe0a70a83fec5a01cded711cb534255a8
Tmuat <Thomas_Muat@hotmail.com>
2021-02-14 10:40:25 +0000

    Added js to add nav background on page load

---
+++ static/js/view_recipe.js
@@ -0,0 +1,5 @@
+
+// JS for Tiny-Slider Carousel in recipe section - from https://github.com/ganlanyuan/tiny-slider
+$(document).ready(function () {
+    $('.navbar').addClass('bg-black-recipe');
+});
