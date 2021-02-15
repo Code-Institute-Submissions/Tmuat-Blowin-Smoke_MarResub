527d919cc327bb0ee6c4029f45e6ab5f908d72d4
Tmuat <Thomas_Muat@hotmail.com>
2021-02-15 10:55:13 +0000

    Updated scipt.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 // When the user scrolls down 30px from the top of the document, add background to navbar
 $(window).scroll(function () {
-    let scroll = $(window).scrollTop();    
+    var scroll = $(window).scrollTop();    
     if (scroll >= 0.1 * $(window).height()) {
         $('.navbar').addClass('bg-black shadow');
     } else {
